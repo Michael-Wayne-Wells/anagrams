eec2f2aa6fff43994c5782e5a2b6c1d2eafecdcc
Michael Wells <mwells1286@gmail.com>
2019-12-13 08:10:45 -0800

    initial commit

---
+++ lib/anagrams.rb
@@ -0,0 +1,8 @@
+
+class Anagrams
+  def initialize
+  end
+  def gram_check()
+
+  end
+end
---
+++ spec/anagrams_spec.rb
@@ -0,0 +1,12 @@
+
+require('rspec')
+require('anagrams')
+
+describe(Anagrams) do
+  describe('#gram_check') do
+    it('test') do
+      test = Anagrams.new()
+      expect(test.gram_check()).to(eq('test'))
+    end
+  end
+end
