b4f528e2db062b234e594b79a9f9373450126af1
Michael Wells <mwells1286@gmail.com>
2019-12-13 08:58:17 -0800

    add method that check if vowels are in words. Test passes.

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -2,9 +2,11 @@ class Anagrams
   def initialize
   end
   def gram_check?(word1,word2)
-
+    if (word1 =~ /[aeiouy]/) & (word2 =~ /[aeiouy]/)
       word_scan = word1.scan(/[#{word2}]/i).join('')
       word_scan.length === word1.length
-  
+    else
+      "please enter a real word"
+    end
   end
 end
