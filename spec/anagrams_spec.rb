d54762c10d187be2a15ad1a3b2a3a85b5629a2e5
Michael Wells <mwells1286@gmail.com>
2019-12-13 09:03:42 -0800

    add test to check if words are antigrams. Also refactors tests to return more specific results

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -5,15 +5,19 @@ describe(Anagrams) do
   describe('#gram_check') do
     it('tests if two words are anagrams') do
       test = Anagrams.new()
-      expect(test.gram_check?("ruby","bury")).to(eq(true))
+      expect(test.gram_check?("ruby","bury")).to(eq("ruby and bury are anagrams!"))
     end
     it('tests if two words are anagrams even if has different capital letters') do
       test = Anagrams.new()
-      expect(test.gram_check?("Ruby","Bury")).to(eq(true))
+      expect(test.gram_check?("Ruby","Bury")).to(eq("Ruby and Bury are anagrams!"))
     end
     it('tests if words contain a vowel') do
       test = Anagrams.new()
       expect(test.gram_check?("ctrb","brct")).to(eq("please enter a real word"))
+    end
+    it('tests if words are an antigram') do
+      test = Anagrams.new()
+      expect(test.gram_check?("hi","bye")).to(eq("hi and bye are antigrams!"))
     end
   end
 end
