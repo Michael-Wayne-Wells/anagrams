f8ff46547d6fe1a4e747951da628da3e2e34be81
Michael Wells <mwells1286@gmail.com>
2019-12-13 08:43:31 -0800

    add method that checks if two words are anagrams. Test passes.

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,7 +1,8 @@
 class Anagrams
   def initialize
   end
-  def gram_check()
-
+  def gram_check?(word1,word2)
+  word_scan = word1.scan(/[#{word2}]/).join('')
+   word_scan.length === word1.length
   end
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -5,7 +5,7 @@ describe(Anagrams) do
   describe('#gram_check') do
     it('test') do
       test = Anagrams.new()
-      expect(test.gram_check()).to(eq('test'))
+      expect(test.gram_check?("ruby","bury")).to(eq(true))
     end
   end
 end
