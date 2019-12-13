ea599f3f1df0cb264c2a5e29dac764c6571cce27
Michael Wells <mwells1286@gmail.com>
2019-12-13 08:57:34 -0800

    add test to check if words contain vowels

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -2,7 +2,9 @@ class Anagrams
   def initialize
   end
   def gram_check?(word1,word2)
-  word_scan = word1.scan(/[#{word2}]/).join('')
-   word_scan.length === word1.length
+
+      word_scan = word1.scan(/[#{word2}]/i).join('')
+      word_scan.length === word1.length
+  
   end
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -3,9 +3,17 @@ require('anagrams')
 
 describe(Anagrams) do
   describe('#gram_check') do
-    it('test') do
+    it('tests if two words are anagrams') do
       test = Anagrams.new()
       expect(test.gram_check?("ruby","bury")).to(eq(true))
+    end
+    it('tests if two words are anagrams even if has different capital letters') do
+      test = Anagrams.new()
+      expect(test.gram_check?("Ruby","Bury")).to(eq(true))
+    end
+    it('tests if words contain a vowel') do
+      test = Anagrams.new()
+      expect(test.gram_check?("ctrb","brct")).to(eq("please enter a real word"))
     end
   end
 end
