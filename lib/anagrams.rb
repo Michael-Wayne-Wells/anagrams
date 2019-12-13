14d9aa8126bb04d853220118d9106ec1c288404e
Michael Wells <mwells1286@gmail.com>
2019-12-13 13:10:27 -0800

    add test fot returning characters when words are not anagagrams or antigrams.

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,30 +1,35 @@
 require('set')
 class Anagrams
+  def initialize
+    @word_bool = false
+  end
 
   def word_check?(word1,word2)
     is_word = Set.new(File.readlines('lib/en.txt').collect(&:chomp))
-    (is_word.include?(word1)) & (is_word.include?(word2))
+    if (is_word.include?(word1)) & (is_word.include?(word2))
+      @word_bool = true
+    end
+    return @word_bool
   end
 
-
-  def gram_check?(word1, word2)
-
-    bool = false
+  def gram_check(word1, word2)
     wordcheck = word1.split(' ') + word2.split(' ')
     wordcheck.each do |x|
       if x =~ /[aeiouy]/i
-        bool = true
+        @word_bool = true
       else
-        bool=true
+        @word_bool=false
         return "please enter a real word"
       end
     end
-    if bool === true
+    if @word_bool === true
       word_scan = word1.scan(/[#{word2}]/i).join('')
       if word_scan.length === word1.length
         "#{word1} and #{word2} are anagrams!"
       elsif word_scan.length === 0
         "#{word1} and #{word2} are antigrams!"
+      else
+        "#{word1} and #{word2} share #{word_scan.length} characters: #{word_scan}"
       end
     end
   end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -5,27 +5,31 @@ describe(Anagrams) do
   describe('#gram_check') do
     it('tests if two words are anagrams') do
       test = Anagrams.new()
-      expect(test.gram_check?("ruby", "bury")).to(eq("ruby and bury are anagrams!"))
+      expect(test.gram_check("ruby", "bury")).to(eq("ruby and bury are anagrams!"))
     end
     it('tests if two words are anagrams even if has different capital letters') do
       test = Anagrams.new()
-      expect(test.gram_check?("Ruby", "Bury")).to(eq("Ruby and Bury are anagrams!"))
+      expect(test.gram_check("Ruby", "Bury")).to(eq("Ruby and Bury are anagrams!"))
     end
     it('tests if words contain a vowel') do
       test = Anagrams.new()
-      expect(test.gram_check?("ctrb", "brct")).to(eq("please enter a real word"))
+      expect(test.gram_check("ctrb", "brct")).to(eq("please enter a real word"))
     end
     it('tests if words are an antigram') do
       test = Anagrams.new()
-      expect(test.gram_check?("hi", "bye")).to(eq("hi and bye are antigrams!"))
+      expect(test.gram_check("hi", "bye")).to(eq("hi and bye are antigrams!"))
     end
     it('tests for anagrams and antigrams while ignoring whitespace') do
       test = Anagrams.new()
-      expect(test.gram_check?("Sharpe", "phra se")).to(eq("Sharpe and phra se are anagrams!"))
+      expect(test.gram_check("Sharpe", "phra se")).to(eq("Sharpe and phra se are anagrams!"))
     end
     it('tests for anagrams and antigrams while ignoring non-letter characters') do
       test = Anagrams.new()
-      expect(test.gram_check?("Sharpe", "phra! se")).to(eq("Sharpe and phra! se are anagrams!"))
+      expect(test.gram_check("Sharpe", "phra! se")).to(eq("Sharpe and phra! se are anagrams!"))
+    end
+    it('returns count of matching characters when not an anagram or antigram') do
+      test = Anagrams.new()
+      expect(test.gram_check("pirate", "party")).to(eq("pirate and party share 4 characters: prat"))
     end
     it('tests for anagrams and antigrams while ignoring non-letter characters') do
       test = Anagrams.new()
