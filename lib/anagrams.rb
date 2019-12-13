6dd264dc70725c7474a9b2a0b3613a8c79831dea
Michael Wells <mwells1286@gmail.com>
2019-12-13 12:04:19 -0800

    add wordlist and add function that will compare if words are in the wordlist.

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,9 +1,14 @@
+require('set')
 class Anagrams
 
-
+  def word_check?(word1,word2)
+    is_word = Set.new(File.readlines('lib/en.txt').collect(&:chomp))
+    (is_word.include?(word1)) & (is_word.include?(word2))
+  end
 
 
   def gram_check?(word1, word2)
+
     bool = false
     wordcheck = word1.split(' ') + word2.split(' ')
     wordcheck.each do |x|
@@ -14,14 +19,13 @@ class Anagrams
         return "please enter a real word"
       end
     end
-
-      if bool === true
-        word_scan = word1.scan(/[#{word2}]/i).join('')
-        if word_scan.length === word1.length
-          "#{word1} and #{word2} are anagrams!"
-        elsif word_scan.length === 0
-          "#{word1} and #{word2} are antigrams!"
-        end
+    if bool === true
+      word_scan = word1.scan(/[#{word2}]/i).join('')
+      if word_scan.length === word1.length
+        "#{word1} and #{word2} are anagrams!"
+      elsif word_scan.length === 0
+        "#{word1} and #{word2} are antigrams!"
+      end
     end
   end
 end
