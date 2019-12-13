0040de13c658802837ead2bdf99e29d20fc00982
Michael Wells <mwells1286@gmail.com>
2019-12-13 09:04:42 -0800

    add function that will check if two words are antigrams. Also adds return phrases of previous results."

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -4,7 +4,11 @@ class Anagrams
   def gram_check?(word1,word2)
     if (word1 =~ /[aeiouy]/) & (word2 =~ /[aeiouy]/)
       word_scan = word1.scan(/[#{word2}]/i).join('')
-      word_scan.length === word1.length
+      if word_scan.length === word1.length
+        "#{word1} and #{word2} are anagrams!"
+      elsif word_scan.length === 0
+        "#{word1} and #{word2} are antigrams!"
+      end
     else
       "please enter a real word"
     end
