78936fe85a825bfd5a6950629fa304bc8f60597f
Michael Wells <mwells1286@gmail.com>
2019-12-13 10:31:29 -0800

    add test to check for anagrams with white space.

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -5,19 +5,24 @@ describe(Anagrams) do
   describe('#gram_check') do
     it('tests if two words are anagrams') do
       test = Anagrams.new()
-      expect(test.gram_check?("ruby","bury")).to(eq("ruby and bury are anagrams!"))
+      expect(test.gram_check?("ruby", "bury")).to(eq("ruby and bury are anagrams!"))
     end
     it('tests if two words are anagrams even if has different capital letters') do
       test = Anagrams.new()
-      expect(test.gram_check?("Ruby","Bury")).to(eq("Ruby and Bury are anagrams!"))
+      expect(test.gram_check?("Ruby", "Bury")).to(eq("Ruby and Bury are anagrams!"))
     end
     it('tests if words contain a vowel') do
       test = Anagrams.new()
-      expect(test.gram_check?("ctrb","brct")).to(eq("please enter a real word"))
+      expect(test.gram_check?("ctrb", "brct")).to(eq("please enter a real word"))
     end
     it('tests if words are an antigram') do
       test = Anagrams.new()
-      expect(test.gram_check?("hi","bye")).to(eq("hi and bye are antigrams!"))
+      expect(test.gram_check?("hi", "bye")).to(eq("hi and bye are antigrams!"))
+    end
+    it('tests for anagrams and antigrams in phrase') do
+      test = Anagrams.new()
+
+      expect(test.gram_check?("Sharpe", "phr ase")).to(eq("Sharpe and phr ase are anagrams!"))
     end
   end
 end
